6f7dbe2eac179079d183f3dbaf98768ecdf8831a
stCarolas <stcarolas@gmail.com>
2017-11-27 00:21:16 +0300

    restructure and add zsh configs

---
+++ overall/zsh/templates/configs/zplug.zsh
@@ -0,0 +1,13 @@
+source ~/.zplug/init.zsh
+
+zplug 'mgee/slimline', as:theme
+
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load --verbose
