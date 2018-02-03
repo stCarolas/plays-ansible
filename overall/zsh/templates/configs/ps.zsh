bc6e98c0fa5bdf6f7fe5abd1b5e56c84c50d5062
stCarolas <stcarolas@gmail.com>
2018-02-03 13:01:09 +0300

    fix zsh line, add vim-slash plugin

--- overall/zsh/templates/configs/ps.zsh
+++
@@ -1,7 +0,0 @@
-function zle-line-init zle-keymap-select {
-    RPS1="${${KEYMAP/vicmd/-- N --}/(main|viins)/-- I--}"$RPS1
-    RPS2=$RPS1$RPS2
-    zle reset-prompt
-}
-zle -N zle-line-init
-zle -N zle-keymap-select
