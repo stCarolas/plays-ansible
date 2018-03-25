acb6e3e74ffe39492c225b2b43e519270be63c84
stCarolas <stcarolas@gmail.com>
2018-03-25 23:53:48 +0300

    fix fzf_open

--- overall/zsh/templates/configs/fzf.zsh
+++ overall/zsh/templates/configs/fzf.zsh
@@ -2,12 +2,31 @@
 source /usr/share/fzf/shell/key-bindings.zsh
 
 # Alt-j to jump in subdir with fzf help
-jump() { BUFFER="cd $(ls -d */ | fzf)"; zle accept-line; }
-zle -N jump
-bindkey "\ej" jump
-#bindkey -s "\ej" 'cd $(ls -d */ | fzf)\n'
+fzf_open() {
+    target=$(ls -d * | fzf)
+    if [ -z "$target" ];
+    then
+        echo "selection is empty"
+    else
+        if [ -d "$target" ];
+        then
+            cd $target
+        else
+            nvim $target
+        fi
+    fi
+}
+fzf_open_zle() {
+    BUFFER="fzf_open";
+    zle accept-line;
+}
+zle -N fzf_open_zle
+bindkey "\ej" fzf_open_zle
 
 # Alt-o to find file and edit with $EDITOR
-edit() { BUFFER="fzf --bind \"enter:execute($EDITOR {})+abort\""; zle accept-line; }
+edit() { 
+    BUFFER="fzf --bind \"enter:execute($EDITOR {})+abort\""; 
+    zle accept-line; 
+}
 zle -N edit
 bindkey "\eo" edit
