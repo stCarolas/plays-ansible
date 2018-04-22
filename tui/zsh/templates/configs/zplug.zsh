b9b85e13f9f5337caa8bbd6254e1fcfccfa0a1e2
stCarolas <stcarolas@gmail.com>
2018-04-22 18:48:41 +0300

    add docker-compose

--- tui/zsh/templates/configs/zplug.zsh
+++ tui/zsh/templates/configs/zplug.zsh
@@ -1,6 +1,7 @@
 source ~/.zplug/init.zsh
 
 zplug 'plugins/git', from:oh-my-zsh
+zplug 'plugins/docker-compose', from:oh-my-zsh
 zplug 'sparsick/ansible-zsh'
 zplug 'jreese/zsh-titles'
 zplug "agkozak/agkozak-zsh-theme"
