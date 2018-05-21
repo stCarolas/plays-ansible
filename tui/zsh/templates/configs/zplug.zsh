eee9bc8d9039b9450f56a0705cd4aa93ac837aed
stCarolas <stcarolas@gmail.com>
2018-05-21 23:53:41 +0300

    add marks functions

--- tui/zsh/templates/configs/zplug.zsh
+++ tui/zsh/templates/configs/zplug.zsh
@@ -5,7 +5,6 @@ zplug 'plugins/docker-compose', from:oh-my-zsh
 zplug 'sparsick/ansible-zsh'
 zplug 'jreese/zsh-titles'
 zplug "agkozak/agkozak-zsh-theme"
-zplug "uvaes/fzf-marks"
 
 # Then, source plugins and add commands to $PATH
 zplug load --verbose
