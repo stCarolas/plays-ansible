b924cd14d9a093d6c6c9693a2730a2e86805fe7c
stCarolas <stcarolas@gmail.com>
2018-05-25 14:46:47 +0300

    change marks file location

--- tui/zsh/templates/configs/marks.zsh
+++ tui/zsh/templates/configs/marks.zsh
@@ -1,5 +1,5 @@
-alias jump='cd $(cat ~/.config/zsh/jumps | fzf | sed -r "s/.*:\ (.*)/\1/")'
+alias jump='cd $(cat ~/.config/jumps | fzf | sed -r "s/.*:\ (.*)/\1/")'
 bindkey -s "^g" "jump\n"
 function mark() {
-    echo "$1: $(pwd)" >> ~/.config/zsh/jumps
+    echo "$1: $(pwd)" >> ~/.config/jumps
 }
